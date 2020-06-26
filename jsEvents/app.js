a7b4276271c94c4b4341f73114826865fe538814
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-26 22:51:28 +0500

    javaScript events

---
+++ jsEvents/app.js
@@ -0,0 +1,16 @@
+// document.getElementById('demo').innerHTML = Date();
+// the above code will change the content of the elements to current date having demo id in it
+
+// document.this.innerHTML = Date();
+// the above code will change the content of its own Element to current Date.
+
+// onclick="displayDate()"
+// the above code show that an event can call js function
+
+// have learnt the following functions:
+// onclick
+// onmouseover
+// onmouseout
+// onkeydown
+// onload
+// onchange
--- jsObjects/app.js
+++ jsObjects/app.js
@@ -17,3 +17,5 @@
 //         return this.firstName + " " + this.lastName;
 //     }
 // };
+// person = person.fullName;
+// alert(person);
