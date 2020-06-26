42b26de3ae199f9168e7521299c5bb8930c657b9
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-26 21:09:26 +0500

    javaScript objects

---
+++ jsObjects/app.js
@@ -0,0 +1,19 @@
+// var car = {type: "fiat", model: "500", color: "white"};
+// var y = car["type"] + " " + car["model"] + " " + car["color"];
+// alert(y);
+
+
+// var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+// var x = person.firstName + " " + person.lastName;
+// alert(x);
+
+// var person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue",
+//     id: "5566",
+//     fullName: function(){
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
