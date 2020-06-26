38190f0be7205f9385d9d5f2abf863d359389e62
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-26 23:05:20 +0500

    javaScript strings types

---
+++ jsString/app.js
@@ -0,0 +1,12 @@
+// var txt = "sljdflsjfldjfslfj";
+// var len = txt.length;
+// alert(len);
+
+// var x = "the great muslim \"leader\" was allama iqbal";
+// alert(x);
+// the above code will insert double qoutes in the String.
+
+
+// var x = "the great muslim \\ was allama iqbal";
+// alert(x);
+// the above code will insert backslash in the String.
