24c4bbde8de954b4d9372f16a59b3684e5694355
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-26 20:15:57 +0500

    js functions

---
+++ jsFunctions/app.js
@@ -0,0 +1,16 @@
+// function myfunction(a, b){
+//     return a * b
+// }
+// var x = myfunction(3, 7)
+// console.log(x)
+
+
+// function toCelsius(f){
+//     return (5 / 9) * (f - 32);
+// }
+// var x = " The temperature today is " + toCelsius(77);
+// console.log(x)
+
+// the number eleven indicates that functions can be used as variables.
+// Local Variables: the local variables are those which we place in the curly brackets {} of functions.
+
