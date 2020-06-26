7f0d13ea2b75ebbc7ba306feda847d2ebdfa647e
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-26 23:14:39 +0500

    javaScript strings methods first commit

---
+++ jsStringMehtod/app.js
@@ -0,0 +1,9 @@
+// var str = "Please locate where 'locate' occurs!";
+// var pos = str.indexOf("locate");
+// alert(pos);
+// the above code will show the index number of first word in most occurance words
+
+// var str = "Please locate where 'locate' occurs!";
+// var pos = str.lastIndexOf("locate");
+// alert(pos);
+// the above code will show the index number of last word in most occurance words
