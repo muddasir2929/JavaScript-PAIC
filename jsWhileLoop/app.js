f701fad9d76cab3ff56a544708b862116d4a86f4
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-28 10:56:58 +0500

    javaScript while loop

---
+++ jsWhileLoop/app.js
@@ -0,0 +1,36 @@
+// var i = 0;
+// var text = " ";
+// while (i < 10) {
+//     text += "The number is " + i + "<br>";
+//     i++;
+// }
+// document.write(text);
+
+// var i = 0;
+// var text = "";
+// do {
+//     text += "the number is " + i + "<br>";
+//     i++;
+// }
+// while (i < 10);
+// console.log(text); 
+
+// var cars = ["BMW", "Volvo", "Saab", "Ford"];
+// var i = 0;
+// var text = "";
+// for (;cars[i];) {
+//   text += cars[i] + "<br>";
+//   i++;
+// } //this is for loop to collect values from arrays
+// document.write(text);
+
+
+// var cars = ["BMW", "Volvo", "Saab", "Ford"];
+// var i = 0;
+// var text = "";
+
+// while (cars[i]) {
+//   text += cars[i] + "<br>";
+//   i++;
+// } //this is while loop to collect values from arrays
+// document.write(text);
