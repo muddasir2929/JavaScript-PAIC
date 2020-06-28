bc0e9396ef54052f7d15a8a1942cbfe8e9b1b0d3
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-28 06:18:55 +0500

    javaScript loop

---
+++ jsLoop/app.js
@@ -0,0 +1,27 @@
+var txt = "";
+for (i = 0; i < 5; i++) {
+    txt += "The number is " + i + "<br>";
+}
+document.write(txt);
+
+
+// var txt = "";
+// var person = {fname:"John", lname:"Doe", age: 25}; 
+// var x;
+// for (x in person) {
+//   txt = person[x] + " ";
+// }
+// console.log(txt);
+
+// var cars = ['BMW', 'Volvo', 'Mini'];
+// var x;
+// for (x of cars) {
+//   document.write(x + "<br >");
+// }
+
+// var cars = "javaScript";
+// var x;
+// for (x of cars) {
+//   document.write(x + "<br >");
+// }
+
