b6c7e5793b6c15a5117c761fabf9d3c4230016fe
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-28 05:50:28 +0500

    javaScript switch method

---
+++ jsSwitch/app.js
@@ -0,0 +1,88 @@
+// switch(expression){
+//     case x:
+//         //code block
+//     case y:
+//         //code block
+//     default case:
+//     //code block
+// }
+// if there is match between epression and code block then it will return, if no match found, then default block of code will run.
+
+
+// switch (new Date().getDay()){
+//     case 0:
+//         day = "sunday";
+//         break;
+//     case 0:
+//         day = "monday";
+//         break;
+//     case 0:
+//             day = "tuesday";
+//         break;
+//     case 0:
+//         day = "wednesday";
+//         break;
+//     case 0:
+//         day = "thursday";
+//         break;
+//     case 0:
+//         day = "friday";
+//         break;
+//     case 0:
+//         day = "sturday";
+//         break;
+// }
+
+
+// switch(new Date().getDay()){
+//     case 0:
+//         weekDay = "sunday";
+//     break;
+//     case 6:
+//         weekDay = "saturday";
+//     break;
+//     default:
+//         weekDay = "looking for weekend";
+// }
+// alert("today is: " + weekDay);
+
+// switch(new Date().getDay()){
+//     default:
+//         weekDay = "looking for weekend";
+//         break;
+//     case 6:
+//         weekDay = "saturday";
+//         break;
+//     case 0:
+//         weekDay = "sunday";
+// }
+// alert("today is: " + weekDay);
+
+
+// switch (new Date().getDay()) {
+//     case 4:
+//     case 5:
+//         text = "Soon it is Weekend";
+//         break;
+//     case 0:
+//     case 6:
+//         text = "It is Weekend";
+//         break;
+//     default:
+//         text = "Looking forward to the Weekend";
+// }
+// alert(text);
+
+// var x = "0";
+// switch (x) {
+//     case 0:
+//         text = "Off";
+//         break;
+//     case 1:
+//         text = "On";
+//         break;
+//     default:
+//         text = "No value found";
+// }
+// alert(text); // answer will be no match found!
+
