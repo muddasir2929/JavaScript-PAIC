e7d7a1b4dbe5bf7d74ea51e23bbb9d0b10473cf8
muddasir2929 <muddasir.mm74@gmail.com>
2020-06-28 11:42:49 +0500

    javaScript break

---
+++ jsBreak/app.js
@@ -0,0 +1,29 @@
+// var text = "";
+// for (var i = 0; i < 10; i++) {
+//     if (i === 5){
+//         break;
+//     }
+//   text += "The number is " + i + "<br>";
+// }  // this is braek statement.
+// document.write(text);
+
+// var text = "";
+// for (var i = 0; i < 10; i++) {
+//     if (i === 5){
+//         continue;
+//     }
+//   text += "The number is " + i + "<br>";
+// }  // this is continue statement.
+// document.write(text);
+
+
+// var cars = ["BMW", "Volvo", "Saab", "Ford"];
+// var text = "";
+// list: {
+//   text += cars[0] + "<br>";
+//   text += cars[1] + "<br>";
+//   break list;
+//   text += cars[2] + "<br>";
+//   text += cars[3] + "<br>";
+// }
+// document.write(text);
