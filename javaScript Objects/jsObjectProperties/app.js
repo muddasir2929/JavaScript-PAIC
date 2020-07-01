7603b6ff9a8ea307dbdc801913d2405ae99f2c4e
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-01 14:41:59 +0500

    javaScript object properties

---
+++ javaScript Objects/jsObjectProperties/app.js
@@ -0,0 +1,14 @@
+// var person = { firstName:"John", lastName: "Doe", age: 50, eyeColor: "blue" };
+// console.log(person.firstName + " is " + person.age + " years old.");
+
+// var person = { firstName:"John", lastName: "Doe", age: 50, eyeColor: "blue" };
+// console.log(person["firstName"] + " is " + person["age"] + " years old.")
+
+// var x = 0;
+// var txt = "";
+// var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
+// for (x in person) {
+//     txt += person[x];
+// }
+// console.log(txt);
+
