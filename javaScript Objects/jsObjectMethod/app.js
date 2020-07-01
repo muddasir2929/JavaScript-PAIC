857b405ea725152509416aa6a24c2cbddd162a69
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-01 15:16:53 +0500

    javaScript object method

---
+++ javaScript Objects/jsObjectMethod/app.js
@@ -0,0 +1,29 @@
+// var person = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     id     : 5566,
+//     fullName : function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   };
+//   console.log(person.fullName());
+// without rounded brackets (). it will return the definition of a function
+
+
+// built in method
+// var message = "Hello world!";
+// var x = message.toUpperCase();
+// alert(x);
+
+
+
+// how to add new method in an object
+// var person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     id: 5566,
+// };
+// person.name = function (){
+//     return this.firstName + " " + this.lastName;
+// };
+// console.log(person);
