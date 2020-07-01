869b460b538de622339bc0bba01aaa50a111c56c
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-01 14:25:40 +0500

    javaScript object definition

---
+++ javaScript Objects/jsDefinition/app.js
@@ -0,0 +1,11 @@
+// objects are variables
+// var person = "john";
+
+// objects are literal
+// var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+// console.log(person);
+
+// var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}
+// var x = person;
+// x.age = 10;           // This will change both x.age and person.age
+// console.log(x);
