336fc34f26ff1cf76d9dc89a552096f93ca490f4
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-03 15:04:10 +0500

    javaScript asseccors

---
+++ Fundamental Javascript/arrayOfJava/app.js
@@ -0,0 +1,23 @@
+// var food = ["burger", "pizza", "sandwitch"];
+// var a = food[2];
+// alert(a);
+
+// var data = {name:"Muhammad",last:"Muddasir",title:"Hafiz"};
+// alert(data["title"]);
+
+// adding elements in an array
+// var foods = [];
+// foods[0] = "Pizza";
+// foods[1] = "Burger";
+// foods[2] = "Snacks";
+// foods[3] = "Sandwich";
+// alert(foods); 
+
+// How much elements are there in an array?
+// var food = ["burger", "pizza", "sandwitch"];
+// alert(food.length);
+
+// push function
+// var food = ["burger", "pizza", "sandwitch"];
+// food.push("snacks");
+// alert(food);
