8902548ef73947421d2f43c7e3ede51862a20451
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-08 15:43:20 +0500

    js strict

--- javaScript Objects/jsConstructor/app.js
+++ javaScript Objects/jsConstructor/app.js
@@ -6,10 +6,10 @@
 //     console.log(y)
 // }
 
-x = 3.14;       // This will not cause an error.
-myFunction();
+// x = 3.14;       // This will not cause an error.
+// myFunction();
 
-function myFunction() {
-  "use strict";
-  y = 3.14;   // This will cause an error
-}
+// function myFunction() {
+//   "use strict";
+//   y = 3.14;   // This will cause an error
+// }
