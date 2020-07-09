93e1da774fdeaf45ff6cf2de6d20af85b5933110
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-09 12:31:10 +0500

    js this

--- javaScript Objects/jsThisKeyword/app.js
+++ javaScript Objects/jsThisKeyword/app.js
@@ -1,12 +1,15 @@
-var person = {
-    firstName: "John",
-    lastName : "Doe",
-    id       : 5566,
-    fullName : function() {
-      return this.firstName + " " + this.lastName;
-    }
-  };
+// var person = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     id       : 5566,
+//     fullName : function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   };
 
-  fullName : function() {
-    return this.firstName + " " + this.lastName;
-  }
+//   fullName : function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+
+"use strict";
+var x = this;
