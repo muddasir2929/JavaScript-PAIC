c02a22368b13a70884405ddb4e9185f8a4c03077
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-13 13:17:40 +0500

    js prototypes

--- javaScript Objects/jsPrototype/app.js
+++ javaScript Objects/jsPrototype/app.js
@@ -1,10 +1,10 @@
-function Person(first, last, age, eyecolor) {
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.eyeColor = eyecolor;
-  }
+// function Person(first, last, age, eyecolor) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eyecolor;
+//   }
   
-  var myFather = new Person("John", "Doe", 50, "blue");
-  var myMother = new Person("Sally", "Rally", 48, "green");
-  console.log(myFather)
+//   var myFather = new Person("John", "Doe", 50, "blue");
+//   var myMother = new Person("Sally", "Rally", 48, "green");
+//   console.log(myFather)
