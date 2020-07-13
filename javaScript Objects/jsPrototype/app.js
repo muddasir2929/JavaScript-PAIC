ebedfeb645ec026e6c43aea8c5326398832ccc18
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-13 14:55:14 +0500

    js prototypes

--- javaScript Objects/jsPrototype/app.js
+++ javaScript Objects/jsPrototype/app.js
@@ -7,4 +7,17 @@
   
 //   var myFather = new Person("John", "Doe", 50, "blue");
 //   var myMother = new Person("Sally", "Rally", 48, "green");
-//   console.log(myFather)
+//   console.log("my father is " + myFather.age + ". My mother is " + myMother.age)
+
+//   function Person(first, last, age, eye) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eye;
+//     this.nationality = "English";
+//   }
+  
+//   var myFather = new Person("John", "Doe", 50, "blue");
+//   var myMother = new Person("Sally", "Rally", 48, "green");
+
+//   console.log("The nationality of my father is " + myFather.nationality + ". The nationality of my mother is " + myMother.nationality)
