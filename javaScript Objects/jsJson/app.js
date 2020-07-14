b8034c117146d4bee5f500ea581ddd705829835f
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-14 14:38:12 +0500

    js JSON

---
+++ javaScript Objects/jsJson/app.js
@@ -0,0 +1,12 @@
+// JSON stands for JavaScript Object Notation
+// JSON is a lightweight data interchange format
+// JSON is language independent *
+// JSON is "self-describing" and easy to understand
+
+// {
+//     "employees":[
+//       {"firstName":"John", "lastName":"Doe"},
+//       {"firstName":"Anna", "lastName":"Smith"},
+//       {"firstName":"Peter", "lastName":"Jones"}
+//     ]
+//     }
