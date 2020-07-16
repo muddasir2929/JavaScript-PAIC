a3523c4e1178dcaf8b75d3dcd4e15966c4bf3bc9
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-16 12:50:02 +0500

    js let

---
+++ Fundamental Javascript/jsLet/app.js
@@ -0,0 +1,17 @@
+var carName = "Volvo";
+
+// code here can use carName
+
+function myFunction() {
+  // code here can also use carName
+}
+
+// code here can NOT use carName
+
+function myFunction() {
+    var carName = "Volvo";
+    // code here CAN use carName
+  }
+  
+  // code here can NOT use carName
+  
