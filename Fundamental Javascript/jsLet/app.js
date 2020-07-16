7895e5a4b6b25b23a2089a2d83ce4c460e961fe6
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-16 12:51:40 +0500

    js let

--- Fundamental Javascript/jsLet/app.js
+++ Fundamental Javascript/jsLet/app.js
@@ -1,35 +1,35 @@
-var carName = "Volvo";
+// var carName = "Volvo";
 
-// code here can use carName
+// // code here can use carName
 
-function myFunction() {
-  // code here can also use carName
-}
+// function myFunction() {
+//   // code here can also use carName
+// }
 
-// code here can NOT use carName
+// // code here can NOT use carName
 
-function myFunction() {
-    var carName = "Volvo";
-    // code here CAN use carName
-  }
+// function myFunction() {
+//     var carName = "Volvo";
+//     // code here CAN use carName
+//   }
   
-  // code here can NOT use carName
+//   // code here can NOT use carName
   
 
-  {
-    var x = 2;
-  }
-  // x CAN be used here
+//   {
+//     var x = 2;
+//   }
+//   // x CAN be used here
 
-  {
-    let x = 2;
-  }
-  // x can NOT be used here
+//   {
+//     let x = 2;
+//   }
+//   // x can NOT be used here
 
-  var x = 10;
-// Here x is 10
-{
-  var x = 2;
-  // Here x is 2
-}
-// Here x is 2
+//   var x = 10;
+// // Here x is 10
+// {
+//   var x = 2;
+//   // Here x is 2
+// }
+// // Here x is 2
