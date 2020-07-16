ba3f56ee0c5264f56d8723674faa2afe945acc87
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-16 12:51:03 +0500

    js let

--- Fundamental Javascript/jsLet/app.js
+++ Fundamental Javascript/jsLet/app.js
@@ -15,3 +15,21 @@ function myFunction() {
   
   // code here can NOT use carName
   
+
+  {
+    var x = 2;
+  }
+  // x CAN be used here
+
+  {
+    let x = 2;
+  }
+  // x can NOT be used here
+
+  var x = 10;
+// Here x is 10
+{
+  var x = 2;
+  // Here x is 2
+}
+// Here x is 2
