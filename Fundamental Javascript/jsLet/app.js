feb26332c6cf52ef2842e0881129be27b9c1d84b
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-17 07:54:33 +0500

    js let

--- Fundamental Javascript/jsLet/app.js
+++ Fundamental Javascript/jsLet/app.js
@@ -35,88 +35,88 @@
 // // Here x is 2
 
 
-var x = 10;
-// Here x is 10
-{
-  let x = 2;
-  // Here x is 2
-}
-// Here x is 10
+// var x = 10;
+// // Here x is 10
+// {
+//   let x = 2;
+//   // Here x is 2
+// }
+// // Here x is 10
 
-var i = 5;
-for (var i = 0; i < 10; i++) {
-  // some statements
-}
-// Here i is 10
-
-
-let i = 5;
-for (let i = 0; i < 10; i++) {
-  // some statements
-}
-// Here i is 5
+// var i = 5;
+// for (var i = 0; i < 10; i++) {
+//   // some statements
+// }
+// // Here i is 10
 
 
-function myFunction() {
-    var carName = "Volvo";   // Function Scope
-  }
-  function myFunction() {
-    let carName = "Volvo";   // Function Scope
-  }
+// let i = 5;
+// for (let i = 0; i < 10; i++) {
+//   // some statements
+// }
+// // Here i is 5
 
 
-  var x = 2;       // Global scope
-  let x = 2;       // Global scope
+// function myFunction() {
+//     var carName = "Volvo";   // Function Scope
+//   }
+//   function myFunction() {
+//     let carName = "Volvo";   // Function Scope
+//   }
 
-  var carName = "Volvo";
-// code here can use window.carName
 
-let carName = "Volvo";
-// code here cannot use window.carName
+//   var x = 2;       // Global scope
+//   let x = 2;       // Global scope
 
-var x = 2;
+//   var carName = "Volvo";
+// // code here can use window.carName
 
-// Now x is 2
+// let carName = "Volvo";
+// // code here cannot use window.carName
+
+// var x = 2;
+
+// // Now x is 2
  
-var x = 3;
+// var x = 3;
 
-// Now x is 3
-
-
-var x = 2;       // Allowed
-let x = 3;       // Not allowed
-
-{
-  var x = 4;   // Allowed
-  let x = 5   // Not allowed
-}
+// // Now x is 3
 
 
-let x = 2;       // Allowed
-let x = 3;       // Not allowed
+// var x = 2;       // Allowed
+// let x = 3;       // Not allowed
 
-{
-  let x = 4;   // Allowed
-  let x = 5;   // Not allowed
-}
-
-
-let x = 2;       // Allowed
-var x = 3;       // Not allowed
-
-{
-  let x = 4;   // Allowed
-  var x = 5;   // Not allowed
-}
+// {
+//   var x = 4;   // Allowed
+//   let x = 5   // Not allowed
+// }
 
 
+// let x = 2;       // Allowed
+// let x = 3;       // Not allowed
 
-let x = 2;       // Allowed
+// {
+//   let x = 4;   // Allowed
+//   let x = 5;   // Not allowed
+// }
 
-{
-  let x = 3;   // Allowed
-}
 
-{
-  let x = 4;   // Allowed
-}
+// let x = 2;       // Allowed
+// var x = 3;       // Not allowed
+
+// {
+//   let x = 4;   // Allowed
+//   var x = 5;   // Not allowed
+// }
+
+
+
+// let x = 2;       // Allowed
+
+// {
+//   let x = 3;   // Allowed
+// }
+
+// {
+//   let x = 4;   // Allowed
+// }
