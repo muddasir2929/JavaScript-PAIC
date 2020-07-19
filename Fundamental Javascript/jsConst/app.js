827637be79e6e9d4dfcbe2cd5f8435eeda1215d0
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-19 22:05:38 +0500

    js const

--- Fundamental Javascript/jsConst/app.js
+++ Fundamental Javascript/jsConst/app.js
@@ -1,17 +1,21 @@
-const PI = 3.141592653589793;
-PI = 3.14;      // This will give an error
-PI = PI + 10;   // This will also give an error
+// const PI = 3.141592653589793;
+// PI = 3.14;      // This will give an error
+// PI = PI + 10;   // This will also give an error
 
-var x = 10;
-// Here x is 10
-{
-  const x = 2;
-  // Here x is 2
-}
-// Here x is 10
+// var x = 10;
+// // Here x is 10
+// {
+//   const x = 2;
+//   // Here x is 2
+// }
+// // Here x is 10
 
 
-const PI;
-PI = 3.14159265359;
+// const PI;
+// PI = 3.14159265359;
 
-const PI = 3.14159265359;
+// const PI = 3.14159265359;
+
+// const PI = 3.141592653589793;
+// PI = 3.14;      // This will give an error
+// PI = PI + 10;   // This will also give an error
