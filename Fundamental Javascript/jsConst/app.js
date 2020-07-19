fb4f7dafc7c4b02466de4dbcb5bf83f1d976d909
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-19 21:59:53 +0500

    js const

---
+++ Fundamental Javascript/jsConst/app.js
@@ -0,0 +1,15 @@
+const PI = 3.141592653589793;
+PI = 3.14;      // This will give an error
+PI = PI + 10;   // This will also give an error
+
+var x = 10;
+// Here x is 10
+{
+  const x = 2;
+  // Here x is 2
+}
+// Here x is 10
+
+
+const PI;
+PI = 3.14159265359;
