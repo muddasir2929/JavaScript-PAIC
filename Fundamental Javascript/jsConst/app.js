e2b8a6d8fcde3da40099e5bf43dfe38bf2d82178
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-20 22:53:09 +0500

    js const

--- Fundamental Javascript/jsConst/app.js
+++ Fundamental Javascript/jsConst/app.js
@@ -28,3 +28,6 @@ car.color = "red";
 
 // You can add a property:
 car.owner = "Johnson";
+
+const car = {type:"Fiat", model:"500", color:"white"};
+car = {type:"Volvo", model:"EX60", color:"red"};    // ERROR
