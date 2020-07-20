58c96c25b1614627e3f04c172fed07ab205c4127
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-20 22:56:11 +0500

    js let

--- Fundamental Javascript/jsConst/app.js
+++ Fundamental Javascript/jsConst/app.js
@@ -21,14 +21,22 @@
 // PI = PI + 10;   // This will also give an error
 
 // You can create a const object:
-const car = {type:"Fiat", model:"500", color:"white"};
+// const car = {type:"Fiat", model:"500", color:"white"};
 
-// You can change a property:
-car.color = "red";
+// // You can change a property:
+// car.color = "red";
 
-// You can add a property:
-car.owner = "Johnson";
+// // You can add a property:
+// car.owner = "Johnson";
 
-const car = {type:"Fiat", model:"500", color:"white"};
-car = {type:"Volvo", model:"EX60", color:"red"};    // ERROR
+// const car = {type:"Fiat", model:"500", color:"white"};
+// car = {type:"Volvo", model:"EX60", color:"red"};    // ERROR
 
+// // You can create a constant array:
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// // You can change an element:
+// cars[0] = "Toyota";
+
+// // You can add an element:
+// cars.push("Audi");
