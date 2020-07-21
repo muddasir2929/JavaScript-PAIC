76ad4a81028b456c3de7099959701cc1d24e28f9
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-21 13:03:07 +0500

    js let

--- Fundamental Javascript/jsConst/app.js
+++ Fundamental Javascript/jsConst/app.js
@@ -42,14 +42,14 @@
 // cars.push("Audi");
 
 
-var x = 2;    //  Allowed
-var x = 3;    //  Allowed
-x = 4;        //  Allowed
+// var x = 2;    //  Allowed
+// var x = 3;    //  Allowed
+// x = 4;        //  Allowed
 
 
-var x = 2;         // Allowed
-const x = 2;       // Not allowed
-{
-  let x = 2;     // Allowed
-  const x = 2;   // Not allowed
-}
+// var x = 2;         // Allowed
+// const x = 2;       // Not allowed
+// {
+//   let x = 2;     // Allowed
+//   const x = 2;   // Not allowed
+// }
