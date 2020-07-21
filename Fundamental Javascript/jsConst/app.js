234c0ab56de9a159368bcb7986766f5d446ba753
muddasir2929 <muddasir.mm74@gmail.com>
2020-07-21 13:02:36 +0500

    js let

--- Fundamental Javascript/jsConst/app.js
+++ Fundamental Javascript/jsConst/app.js
@@ -45,3 +45,11 @@
 var x = 2;    //  Allowed
 var x = 3;    //  Allowed
 x = 4;        //  Allowed
+
+
+var x = 2;         // Allowed
+const x = 2;       // Not allowed
+{
+  let x = 2;     // Allowed
+  const x = 2;   // Not allowed
+}
