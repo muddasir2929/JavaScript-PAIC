d03571b7857d13eafc5d602d33bb12d8aafb961b
muddasir2929 <muddasir.mm74@gmail.com>
2020-08-03 21:34:28 +0500

    js function

--- Fundamental Javascript/jsFunctions/app.js
+++ Fundamental Javascript/jsFunctions/app.js
@@ -24,3 +24,9 @@
 //   "Volvo",
 //   "BMW"
 // ];
+
+
+
+var x = 5;         // assign the value 5 to x
+var y = 2;         // assign the value 2 to y
+var z = x + y;     // assign the value 7 to z (x + y)
