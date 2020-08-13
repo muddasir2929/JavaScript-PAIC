d69e15af4d29d5c276553e70f8433428e6d09a68
muddasir2929 <muddasir.mm74@gmail.com>
2020-08-14 00:16:53 +0500

    js function

--- Fundamental Javascript/jsFunctions/app.js
+++ Fundamental Javascript/jsFunctions/app.js
@@ -121,8 +121,8 @@
 // var person = "John Doe";
 // var answer = 'Yes I am!';
 
-var price1 = 5;
-var price2 = 6;
+// var price1 = 5;
+// var price2 = 6;
 // var total = price1 + price2;
 
 // var price1 = 5;
