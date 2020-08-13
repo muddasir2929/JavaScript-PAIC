96778c03770792ce44023801329738da759ba670
muddasir2929 <muddasir.mm74@gmail.com>
2020-08-14 00:16:26 +0500

    js function

--- Fundamental Javascript/jsFunctions/app.js
+++ Fundamental Javascript/jsFunctions/app.js
@@ -123,8 +123,8 @@
 
 var price1 = 5;
 var price2 = 6;
-var total = price1 + price2;
+// var total = price1 + price2;
 
-var price1 = 5;
+// var price1 = 5;
 // var price2 = 6;
 // var total = price1 + price2;
