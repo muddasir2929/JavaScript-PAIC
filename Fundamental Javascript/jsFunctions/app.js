aec7a17f934c8a04aa71faf07720f56815d33e23
muddasir2929 <muddasir.mm74@gmail.com>
2020-08-15 13:42:24 +0500

    js function

--- Fundamental Javascript/jsFunctions/app.js
+++ Fundamental Javascript/jsFunctions/app.js
@@ -128,3 +128,10 @@
 // var price1 = 5;
 // var price2 = 6;
 // var total = price1 + price2;
+
+// var price1 = 5;
+// var price2 = 6;
+// var total = price1 + price2;
+// var pi = 3.14;
+// var person = "John Doe";
+// var answer = 'Yes I am!';
