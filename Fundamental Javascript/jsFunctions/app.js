e8c33059131ab4a66bd05b01f6ad74682f096b2b
muddasir2929 <muddasir.mm74@gmail.com>
2020-08-27 10:48:07 +0500

    js function

--- Fundamental Javascript/jsFunctions/app.js
+++ Fundamental Javascript/jsFunctions/app.js
@@ -205,3 +205,16 @@
 // {
 //   const x = 2;
 //   // Here x is 2
+
+
+
+// var x = 10;
+// // Here x is 10
+// {
+//   const x = 2;
+//   // Here x is 2
+// var x = 10;
+// // Here x is 10
+// {
+//   const x = 2;
+//   // Here x is 2
