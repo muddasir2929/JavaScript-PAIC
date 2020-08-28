937e84c035e61fdbab5133ebd483ba3a620288c6
muddasir2929 <muddasir.mm74@gmail.com>
2020-08-28 09:46:31 +0500

    js function

--- javaScript Objects/jsObjectMethod/app.js
+++ javaScript Objects/jsObjectMethod/app.js
@@ -27,3 +27,15 @@
 //     return this.firstName + " " + this.lastName;
 // };
 // console.log(person);
+
+
+// how to add new method in an object
+// var person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     id: 5566,
+// };
+// person.name = function (){
+//     return this.firstName + " " + this.lastName;
+// };
+// console.log(person);
